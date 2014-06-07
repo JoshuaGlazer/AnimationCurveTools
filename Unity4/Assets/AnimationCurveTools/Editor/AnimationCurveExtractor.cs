a158e83e64b430564fac9e47ec74da4bd83217a3
JoshuaGlazer <vbunny@alum.mit.edu>
2014-06-07 09:55:52 -0700

    Unity >= 4.3 support and some test data
    Thanks Aurelio Provedo!

--- Unity4/Assets/AnimationCurveTools/Editor/AnimationCurveExtractor.cs
+++ Unity4/Assets/AnimationCurveTools/Editor/AnimationCurveExtractor.cs
@@ -1,3 +1,6 @@
+
+// Original code by: Joshua Glazer 
+// Updated to Unity 4.3.4 by: Aurelio Provedo [aurelioprovedo@gmail.com]
 using System;
 using UnityEngine;
 using UnityEditor;
@@ -5,9 +8,9 @@ using UnityEditor;
 
 public class AnimationCurveExtractor : EditorWindow
 {
-
 	
-	public void Init( SerializedProperty inTargetProperty )
+	
+	public void Init(SerializedProperty inTargetProperty)
 	{
 		//keep the iterator in its current state...
 		_PopupTargetAnimationCurveProperty = inTargetProperty;
@@ -15,19 +18,32 @@ public class AnimationCurveExtractor : EditorWindow
 	
 	void OnGUI()
 	{
-		AnimationClip anim = EditorGUILayout.ObjectField( "Source Animation", _SourceAnimationClip, typeof( AnimationClip ), false ) as AnimationClip;
+		AnimationClip anim = EditorGUILayout.ObjectField("Source Animation", _SourceAnimationClip, typeof(AnimationClip), false) as AnimationClip;
 		
-		if( anim != _SourceAnimationClip )
+		if (anim != _SourceAnimationClip)
 		{
-			if( anim != null )
+			_SourceAnimationClip = anim;
+			if (anim != null)
 			{
-				_Curves = AnimationUtility.GetAllCurves ( anim );
+				#if !UNITY_4_2
+				_Curves = AnimationUtility.GetCurveBindings(anim);
+				#else
+				_Curves = AnimationUtility.GetAllCurves(anim);
+				#endif
+				
 				_SelectedCurveIndex = 0;
 				int curveCount = _Curves.Length;
-				_CurveNames = new string[ curveCount ];
-				for( int i = 0; i < curveCount; ++i )
+				_CurveNames = new string[curveCount];
+				for (int i = 0; i < curveCount; ++i)
 				{
-					_CurveNames[ i ] = _Curves[ i ].path + "/" + _Curves[ i ].propertyName;
+					if (_Curves[i].path == "")
+					{
+						_CurveNames[i] = _Curves[i].propertyName;
+					}
+					else
+					{
+						_CurveNames[i] = _Curves[i].path + "/" + _Curves[i].propertyName;
+					}
 				}
 			}
 			else
@@ -35,78 +51,87 @@ public class AnimationCurveExtractor : EditorWindow
 				_Curves = null;
 				_CurveNames = null;
 			}
-			
-			_SourceAnimationClip = anim;
 		}
 		
-		if( _Curves != null && _Curves.Length > 0 )
+		if (_Curves != null && _Curves.Length > 0)
 		{
-			_SelectedCurveIndex = EditorGUILayout.Popup( "Source Curve", _SelectedCurveIndex, _CurveNames );
-		
-			EditorGUILayout.CurveField( "Data", _Curves[ _SelectedCurveIndex ].curve );
-		
-			/*
-			_ShouldZeroOriginalCurve = EditorGUILayout.Toggle( "Should Zero Original Curve", _ShouldZeroOriginalCurve );
-			*/
+			_SelectedCurveIndex = EditorGUILayout.Popup("Source Curve", _SelectedCurveIndex, _CurveNames);
 			
-			if( GUILayout.Button( "Extract!" ) )
+			#if !UNITY_4_2
+			EditorGUILayout.CurveField("Data", AnimationUtility.GetEditorCurve(_SourceAnimationClip, _Curves[_SelectedCurveIndex]));
+			#else
+			EditorGUILayout.CurveField("Data", _Curves[_SelectedCurveIndex].curve);
+			#endif
+			
+			/*
+            _ShouldZeroOriginalCurve = EditorGUILayout.Toggle( "Should Zero Original Curve", _ShouldZeroOriginalCurve );
+            */
+			
+			if (GUILayout.Button("Extract!"))
 			{
 				Extract();
-				Close ();
+				Close();
 			}
 		}
-			
+		
 		
 		
 	}
 	
 	private void Extract()
 	{
-		AnimationCurve sourceCurve = _Curves[ _SelectedCurveIndex ].curve;
+		#if !UNITY_4_2
+		AnimationCurve sourceCurve = AnimationUtility.GetEditorCurve(_SourceAnimationClip, _Curves[_SelectedCurveIndex]);
+		#else
+		AnimationCurve sourceCurve = _Curves[_SelectedCurveIndex].curve;
+		#endif
 		
-		_PopupTargetAnimationCurveProperty.animationCurveValue = AnimationCurveCopier.CreateCopy( sourceCurve );
+		_PopupTargetAnimationCurveProperty.animationCurveValue = AnimationCurveCopier.CreateCopy(sourceCurve);
 		_PopupTargetAnimationCurveProperty.serializedObject.ApplyModifiedProperties();
 		
 		/*
-		 *we would need to copy this back in if we want it to work...
-		if( _ShouldZeroOriginalCurve )
-		{
-			Keyframe[] keys = sourceCurve.keys;
-			for( int i = 0, c = keys.Length; i < c; ++i )
-			{
-				keys[ i ].value = 0;
-			}
-			sourceCurve.keys = keys;
-		}
-		*/
+         *we would need to copy this back in if we want it to work...
+        if( _ShouldZeroOriginalCurve )
+        {
+            Keyframe[] keys = sourceCurve.keys;
+            for( int i = 0, c = keys.Length; i < c; ++i )
+            {
+                keys[ i ].value = 0;
+            }
+            sourceCurve.keys = keys;
+        }
+        */
 	}
 	
-	private SerializedProperty			_PopupTargetAnimationCurveProperty;
+	private SerializedProperty _PopupTargetAnimationCurveProperty;
 	
-	private AnimationClip				_SourceAnimationClip;
+	private AnimationClip _SourceAnimationClip;
 	
-	private AnimationClipCurveData[]	_Curves;
-	private string[]					_CurveNames;
-	private int							_SelectedCurveIndex;
+	#if !UNITY_4_2
+	private EditorCurveBinding[] _Curves;
+	#else
+	private AnimationClipCurveData[] _Curves;
+	#endif
+	
+	private string[] _CurveNames;
+	private int _SelectedCurveIndex;
 	//private bool						_ShouldZeroOriginalCurve;
-	
-	
-	
 }
+
 
 public static class AnimationCurveCopier
 {
-	public static void Copy( AnimationCurve inSource, AnimationCurve inDest )
+	public static void Copy(AnimationCurve inSource, AnimationCurve inDest)
 	{
 		inDest.keys = inSource.keys;
 		inDest.preWrapMode = inSource.preWrapMode;
 		inDest.postWrapMode = inSource.postWrapMode;
 	}
 	
-	public static AnimationCurve CreateCopy( AnimationCurve inSource )
+	public static AnimationCurve CreateCopy(AnimationCurve inSource)
 	{
 		AnimationCurve newCurve = new AnimationCurve();
-		Copy( inSource, newCurve );
+		Copy(inSource, newCurve);
 		return newCurve;
 	}
 }
